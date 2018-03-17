f2045106ea32ed0e1bd732c771c03cbadd5c9725
Зюзин Максим Сергеевич <zms@main.sgu.ru>
2018-03-17 11:38:48 +0400

    Lecture examples

---
+++ Lecture/Boxing/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable obj1 = new Message();
+            obj1.Print();
+
+            IHttpServlet obj2 = new Message();
+            obj2.DoPost();
+
+
+            // просходит упаковка, т.к. Twit - это структура
+            // и приводим к её ссылочному типу
+            IHttpServlet obj3 = new Twit();
+
+            Console.WriteLine(Max(3, 5));
+            Console.WriteLine(Max(3.5, 2.8));
+
+            //var q = new Lecture.Program();
+            //Lecture.Program.Main(new { "Added references"});
+            Lecture.Program.Main(new string[] { "Added references" });
+        }
+
+        interface IPrintable
+        {
+            void Print();
+        }
+
+        interface IHttpServlet
+        {
+            void DoGet();
+            void DoPost();
+        }
+
+        class Message : IPrintable, IHttpServlet
+        {
+            // явная реализация интерфейса
+            public void Print()
+            {
+
+            }
+
+            void IHttpServlet.DoGet()
+            {
+
+            }
+
+            // не можем использовать какие-либо спецификаторы, т.к. в этом отношении это бесспысленно
+            void IHttpServlet.DoPost()
+            {
+                Console.WriteLine("IHttpServlet.DoPost works");
+            }
+        }
+
+        struct Twit : IHttpServlet
+        {
+            public void DoGet()
+            {
+                Console.WriteLine("DoGet method works");
+            }
+            public void DoPost()
+            {
+                Console.WriteLine("DoPost method works");
+            }
+        }
+
+        static T Max<T>(T x, T y) where T: IComparable<T>
+        {
+            return x.CompareTo(y) > 0 ? x : y;
+        }
+    }
+}
---
+++ Lecture/Lecture/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lecture
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Закрываем обобщение, т.е.
+             * создаём конкретный экземляр объекта(функции или класса)
+             * согласно шаблону
+             */
+            MyClass<int> c = new MyClass<int>();
+            MyClass<double> x = new MyClass<double>();
+            MyClass<int> q = new MyClass<int>();
+            MyClass<float> a = new MyClass<float>();
+
+            Parent obj1 = new Parent();
+            Parent obj2 = new Child(); // ссылочная совместимость
+
+            Storage<Parent> storage1 = new Storage<Parent>();
+            // Storage<Parent> storage2 = new Storage<Child>(); // не можем так сделать,
+            // т.к. Storage<Parent> и Storage<Child> разные классы
+        }
+
+        class Counter
+        {
+            protected static int cnt;
+        }
+        class MyClass<T> : Counter
+        {
+            //private static int cnt;
+
+            public MyClass()
+            {
+                cnt++;
+                Console.WriteLine(cnt);
+            }
+        }
+
+        class Parent
+        {
+            public int a;
+        }
+
+        class Child : Parent
+        {
+            public int c;
+        }
+        class Storage<T> where T: class
+        {
+            //TODO
+        }
+
+
+        // нельзя наследоваться от джинериков
+        // Открываем обобщение
+        class ExampleClassToShowForbiddenInheritance<T> where T : new()
+        {
+            T[] array;
+            const int n = 3;
+            public ExampleClassToShowForbiddenInheritance()
+            {
+                array = new T[n];
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = new T();
+                }
+            }
+
+            //public ExampleClassToShowForbiddenInheritance
+        }
+
+        // но нельзя так
+        // class ThisClassWantsToInheritAnotherClass:ExampleClassToShowForbiddenInheritance<Int32>
+        // и нельзя так
+        // class ThisClassWantsToInheritAnotherClass<T> : ExampleClassToShowForbiddenInheritance<T>
+        // но можно так
+        // class ThisClassWantsToInheritAnotherClass<T>:ExampleClassToShowForbiddenInheritance<Int32>
+        /*class ThisClassWantsToInheritAnotherClass<T> : ExampleClassToShowForbiddenInheritance<T>
+        {
+            public ThisClassWantsToInheritAnotherClass()
+            {
+
+            }
+
+            // 
+        }*/
+
+    }
+}
